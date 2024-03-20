bcf72c62ca78c2f2e6d53b88f5bebc3e3e92cd57
rahulanand007 <rahul03061998@gmail.com>
2024-03-20 23:04:19 +0530

    operators

---
+++ dataTypesAndVariables.py
@@ -0,0 +1,24 @@
+print("Hello World!!")
+
+#type function tells the type of the data
+print(type("Hello world"))
+
+
+
+#Numeric Data types
+type(5) #int
+
+print(type(2.34)) #float - No.'s with decimal point
+
+type(True) #Boolean
+type(False) #Boolean
+
+
+print(type(None)) #NoneType
+
+
+name =input()
+
+print(name)
+
+variab = int(input()) #Typecasting -changing the type of data
---
+++ operators.py
@@ -0,0 +1,31 @@
+
+# % - modulus gives remainder
+#     when x % y and x is negative the remainder will be y-(x%y) 
+print(-10%3)
+
+# x**y give x raised to the power y
+print(5**2)
+
+#x//y gives onlt the floor value of result
+
+print(15//2)
+
+
+################################
+
+# Operator Precedence - B - Bracket
+#                       E - Exponential
+#                       D - Division
+#                       M - Multiplication/ Modulus
+#                       A - Addition
+#                       S - 
+
+
+# bool typecasting 
+print(bool(20),bool(-5.5),bool(0))
+
+print(bool("hello"),bool("false"),bool(""))
+
+print(not(10>9),"------------------")
+
+
