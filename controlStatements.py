b5be81723e62ee1a85eb31541fbb27c7478267aa
rahulanand007 <rahul03061998@gmail.com>
2024-03-23 20:21:34 +0530

    Control Statements

---
+++ controlStatements.py
@@ -0,0 +1,18 @@
+# if - else in python
+
+x = 10
+
+if x>=10:
+    print("x is a multiple digit number");
+else:
+    print("x is a single digit number")
+
+
+
+
+num = int(input())
+
+if num%2 == 0:
+    print("number is even");
+else:
+    print("number is odd");
