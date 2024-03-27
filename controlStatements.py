e2b82811cd49041cf40a493b0bb72547db4676d2
rahulanand007 <rahul03061998@gmail.com>
2024-03-27 23:08:10 +0530

    loops

--- controlStatements.py
+++ controlStatements.py
@@ -1,18 +1,23 @@
-# if - else in python
+# # if - else in python
 
-x = 10
+# x = 10
 
-if x>=10:
-    print("x is a multiple digit number");
-else:
-    print("x is a single digit number")
-
-
+# if x>=10:
+#     print("x is a multiple digit number");
+# else:
+#     print("x is a single digit number")
 
 
-num = int(input())
 
-if num%2 == 0:
-    print("number is even");
-else:
-    print("number is odd");
+
+# num = int(input())
+
+# if num%2 == 0:
+#     print("number is even");
+# else:
+#     print("number is odd");
+
+a =9
+
+if (a%2!=0):
+      print("TRUE")
---
+++ loops.py
@@ -0,0 +1,33 @@
+# print("this is a statement", end="hksdAHKDJH")
+# print("2nd statment")
+
+
+# i=1 
+
+# while i<=1000:
+#     print(i,end=" ")
+#     i+=1
+
+# i=1
+
+# while i<10:
+#     if i%2 !=0:
+#         print(i)
+
+#     i+=1    
+
+
+# range(5) means 0 to 4
+print(*range(5))
+
+#range(2,5,1) - range(start,end,increment)
+print(*range(2,15,2))
+
+#range(2,5) - range(start,end)
+print(*range(2,5))
+
+
+
+#for loops
+for i in range(20,50):
+    print(i)
