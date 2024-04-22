8fda1128b75922ca8c966bf8b0c823e4e8923600
rahulanand007 <rahul03061998@gmail.com>
2024-04-22 21:20:07 +0530

    factors

--- DSA/factors.py
+++ DSA/factors.py
@@ -33,4 +33,28 @@ def checkPrime(N):
     else:
         return False
     
-print(checkPrime(2))
+# print(checkPrime(2))
+
+
+
+def solve(A):
+        sum = 0
+
+        for i in range(1,int(A**0.5)+1):
+            print(sum,A,i)
+            print("---------")
+            if A%i ==0:
+                if i==1:
+                    sum+=1
+                elif i == A/i:
+                    sum +=i
+                else:
+                    sum+= i +A/i    
+        print(sum)
+        if int(sum) == A:
+            return 1
+        else:
+            return 0   
+        
+
+# print(solve(1))
